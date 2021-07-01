b1310cdcdf6ea7f6e9d2adcb95f88b9b8476bad8
godfrey0 <80189918+godfrey0@users.noreply.github.com>
2021-07-01 22:11:04 +0800

    Added js and css

---
+++ script.js
@@ -0,0 +1,26 @@
+
+let pics = [
+    "GI WhatsApp.png", //0
+    "GI Pay Now.png", //1
+    "GI QR code Scan & Pay.jpg", //2
+    "GI Hours.png", //3
+    "GI Email.png", //4
+    "GI Collection.png", //5
+    "GI 151NTP.png", //6
+    "GI Road Map.jpg", //7
+    "GI QR code Webpage.jpg", //8
+    ]
+      let btn = document.querySelector("button");
+      let img = document.querySelector("img");
+      let counter = 1;
+      
+      btn.addEventListener("click", function(){
+       if (counter === 9)
+       {
+        counter = 0;
+        }
+        img.src = pics[counter]
+        counter = counter +1;
+  
+      });
+  
