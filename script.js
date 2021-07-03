8dc790d35afef193f8bcaa2636a800a1d36f505a
godfrey0 <80189918+godfrey0@users.noreply.github.com>
2021-07-03 23:49:39 +0800

    modified script.js

--- script.js
+++ script.js
@@ -1,21 +1,26 @@
 
 let pics = [
-    "GI WhatsApp.png", //0
-    "GI Pay Now.png", //1
-    "GI QR code Scan & Pay.jpg", //2
-    "GI Hours.png", //3
-    "GI Email.png", //4
-    "GI Collection.png", //5
-    "GI 151NTP.png", //6
-    "GI Road Map.jpg", //7
-    "GI QR code webpage.jpg", //8
+    "GI Road Map.jpg", //0
+    "GI QR code Webpage.jpg", //1
+    "GI 10.jpg", //2
+    "GI 11.jpg", //3
+    "GI 12.jpg", //4
+    "GI 13.jpg", //5
+    "GI WhatsApp.png", //6
+    "GI Pay Now.png", //7
+    "GI QR code Scan & Pay.jpg", //8
+    "GI Hours.png", //9
+    "GI Email.png", //10
+    "GI Collection.png", //11
+    "GI 151NTP.png", //12
+    
     ]
       let btn = document.querySelector("button");
       let img = document.querySelector("img");
       let counter = 1;
       
       btn.addEventListener("click", function(){
-       if (counter === 9)
+       if (counter === 13)
        {
         counter = 0;
         }
